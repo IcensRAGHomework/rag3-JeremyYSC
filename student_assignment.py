389a0c2e3315368822b391e588c2a73291a9dca8
jeremyys_chen <jeremyys_chen@asus.com>
2025-03-11 03:08:25 +0800

    hw03_1 ver.13

--- student_assignment.py
+++ student_assignment.py
@@ -18,8 +18,8 @@ def generate_hw01():
 
     openai_ef = embedding_functions.OpenAIEmbeddingFunction(
         api_key=gpt_emb_config['api_key'],
-        api_base=gpt_emb_config['api_base'],
         api_type=gpt_emb_config['openai_type'],
+        api_base=gpt_emb_config['api_base'],
         api_version=gpt_emb_config['api_version'],
         deployment_id=gpt_emb_config['deployment_name']
     )
@@ -43,31 +43,7 @@ def generate_hw01():
     documents = df["HostWords"].tolist()
 
     for index, row in df.iterrows():
-        # try:
         timestamp = int(datetime.strptime(row["CreateDate"], "%Y-%m-%d").timestamp())
-            # print(timestamp)
-        # except (ValueError, TypeError):
-        #     timestamp = 0
-
-        # city_csv = str(row.get("City", ""))
-        # town_csv = str(row.get("Town", ""))
-        # address = row["Address"]
-        # city, town = parse_city_town(address)
-        #
-        # if city_csv != city:
-        #     print("city_csv: " + city_csv)
-        #     print("city: " + city)
-        #
-        # if town_csv != town:
-        #     print("town_csv: " + town_csv)
-        #     print("town: " + town)
-
-        # if not is_valid_city(city) or not is_valid_town(town):
-        #     print(city)
-        #     print(town)
-        #     city_from_address, town_from_address = parse_city_town(address)
-        #     city = city_from_address if not is_valid_city(city) else city
-        #     town = town_from_address if not is_valid_town(town) else town
 
         city = re.match(city_pattern, row["Address"]).group()
 
@@ -131,32 +107,6 @@ def demo(question):
 
     print(collection)
     return collection
-
-
-def is_valid_city(city):
-    return bool(city and isinstance(city, str) and ("縣" in city or "市" in city))
-
-
-def is_valid_town(town):
-    return bool(town and isinstance(town, str) and any(x in town for x in ["鄉", "鎮", "區", "市"]))
-
-
-def parse_city_town(address):
-    if not address or pd.isna(address):
-        return "", ""
-
-    first_address = address.split('/')[0].strip()
-
-    city_pattern = r"([^縣市]+[縣市])"
-    town_pattern = r"[縣市]([^鄉鎮區市]+[鄉鎮區市])"
-
-    city_match = re.search(city_pattern, first_address)
-    town_match = re.search(town_pattern, first_address)
-
-    city = city_match.group(0) if city_match else ""
-    town = town_match.group(1) if town_match else ""
-
-    return city, town
 
 
 def main():
